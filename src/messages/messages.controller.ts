fe1bc43b96519effe9f712becc09da8784359280
Nour <fllstknour7@gmail.com>
2026-04-08 21:13:18 +0200

    08APR26--before setup validation

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessage() {}
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log(body);
+  }
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
+}
