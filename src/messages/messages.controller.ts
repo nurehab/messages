7664c8199f65ff18449f49be89deb18f96cc2f7f
Nour <fllstknour7@gmail.com>
2026-04-09 11:05:46 +0200

    09APR26-after setup valid

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
   listMessage() {}
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log(body);
   }
   @Get('/:id')
